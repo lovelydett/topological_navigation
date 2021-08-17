111e6f6616644ba79043980e656bc521b516a281
lovelydett <441875808@qq.com>
2021-08-17 17:19:51 +0800

    clean

--- cmake-build-debug/catkin_generated/setup_cached.sh
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -8,7 +8,5 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/tt/catkin_ws/src/topological_navigation/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/tt/catkin_ws/src/topological_navigation/cmake-build-debug/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/tt/catkin_ws/src/topological_navigation/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/tt/catkin_ws/src/topological_navigation/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/tt/catkin_ws/src/topological_navigation:$ROS_PACKAGE_PATH"
