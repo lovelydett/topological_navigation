248cf3a7f8668e265c3d604a8a5bc8745a8fcb17
lovelydett <441875808@qq.com>
2021-08-17 17:22:59 +0800

    .gitignore

--- cmake-build-debug/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/tt/catkin_ws/src/topological_navigation/cmake-build-debug/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
