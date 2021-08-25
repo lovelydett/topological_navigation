d8f59e3b2fb3c18ec1fa08c54fef959ef2973346
lovelydett <441875808@qq.com>
2021-08-25 16:19:35 +0800

    stop printing pos update log

--- src/topological_nav.cpp
+++ src/topological_nav.cpp
@@ -73,8 +73,8 @@ private:
           current_pos_.y = transform.getOrigin().y();
           // current_pos_.z = transform.getOrigin().z();
           current_pos_.z = 0.f;
-          ROS_INFO("current pos updated: %.2f, %.2f", current_pos_.x,
-                   current_pos_.y);
+          // ROS_INFO("current pos updated: %.2f, %.2f", current_pos_.x,
+          //         current_pos_.y);
           current_pos_callback();
         } catch (const tf::TransformException &exception) {
           ROS_WARN("time-out for tf msg: %s", exception.what());
